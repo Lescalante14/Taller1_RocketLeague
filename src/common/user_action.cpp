86ab13042974c3225b44c2a5eb23b686c988ff29
spenalva1 <penalvasantiagogm@gmail.com>
2022-11-21 21:17:37 -0300

    serialize y constructor de UserAction corregidos

--- src/common/user_action.cpp
+++ src/common/user_action.cpp
@@ -1,24 +1,28 @@
 #include "user_action.h"
 #include <sstream>
 
+
+#define USER_ACTION_SIZE 2
+
 UserAction::UserAction(uint8_t _code, uint8_t _car_id)
     : code(_code), car_id(_car_id) { }
 
 UserAction::UserAction(std::string &action) {
-    std::stringstream ss(action);
-    int codei;
-    ss >> codei;
-    this->code = codei;
+    const char* buf = action.c_str();
+    this->code = buf[0];
+    this->car_id = buf[1];
+}
+
+std::string UserAction::serialize() {
+    char buf[USER_ACTION_SIZE];
+    buf[0] = this->code;
+    buf[1] = this->car_id;
+    std::string message(buf, USER_ACTION_SIZE);
+    return message;
 }
 
 bool UserAction::is(uint8_t _code) {
     return this->code == _code;
-}
-
-std::string UserAction::serialize() {
-    std::stringstream ss;
-    ss << this->code;
-    return ss.str();
 }
 
 void UserAction::set_car_id(uint8_t id) {
