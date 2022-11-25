48292ebc3638547f2d95999cd097378e165d6426
Luis Escalante <lescalante@fi.uba.ar>
2022-11-24 21:11:59 -0300

    Fix update of model client

--- src/client/presentation/ClientMatch.cpp
+++ src/client/presentation/ClientMatch.cpp
@@ -4,30 +4,12 @@
 
 #include "ClientMatch.h"
 
-ClientMatch::ClientMatch(ClientMatchState state, SDL2pp::Renderer &renderer, MatchSetup setup)
-: state(state), field(renderer), matchSetup(std::move(setup)), positionConverter(matchSetup),
+ClientMatch::ClientMatch(ClientMatchState state, SDL2pp::Renderer &renderer, MatchSetup &setup)
+: state(state), field(renderer), matchSetup(setup), positionConverter(matchSetup),
 ball(ClientBallState(state.get_ball_position_x(renderer),state.get_ball_position_y(renderer),0), renderer)
 {
     for (auto &car : state.get_cars()) {
         cars.emplace_back(ClientCar(car, renderer));
-    }
-}
-
-void ClientMatch::update(ClientMatchState s, SDL2pp::Renderer &renderer) {
-	this->field.~Field();
-	this->ball.~ClientBall();
-	this->state.~ClientMatchState();
-	
-	this->field = Field(renderer);
-	this->ball = ClientBall(ClientBallState(s.get_ball_position_x(renderer),
-											s.get_ball_position_y(renderer),
-											0), 
-							renderer);
-	this->state = s;
-	cars.clear();
-
-	for (auto &car : state.get_cars()) {
-		cars.emplace_back(ClientCar(car, renderer));
     }
 }
 
