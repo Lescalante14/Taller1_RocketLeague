e77fc02d1cd999ba72a0ee494e960d59754a1277
Mario Adris <madris@fi.uba.ar>
2022-11-30 00:07:45 -0300

    adding installation scripts

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+if [ ! -d "./build" ]
+then
+	echo "'build' directory does not exist." 
+
+elif [ ! -f "./build/src/rl_server" ] || 
+     [ ! -f "./build/src/rocket_league" ]
+then
+	echo "first run ./build.sh"
+
+elif [ ! -f "./.rl_config.yml" ] ||
+     [ ! -d "./src/client/assets" ]
+then
+		echo "some files are missing, re-clone this repo..."
+
+else
+	# install the apps 
+	sudo cp ./build/src/rl_server /usr/bin/rl_server
+	sudo cp ./build/src/rocket_league /usr/bin/rocket_league
+
+	# create config dir 
+	if [ ! -d "/etc/rocket_league" ]
+	then
+		sudo mkdir /etc/rocket_league
+	fi
+	
+	sudo cp ./.rl_config.yml /etc/rocket_league/.rl_config.yml
+
+
+	# create data dir 
+	if [ ! -d "/var/rocket_league" ]
+	then
+		sudo mkdir /var/rocket_league
+	fi
+
+	sudo cp ./src/client/assets/* /var/rocket_league/
+
+	echo "Installation complete"
+fi
--- src/server/b2d_model/game_model.cpp
+++ src/server/b2d_model/game_model.cpp
@@ -31,7 +31,7 @@ GameModel::GameModel(size_t cars_amount, size_t _step_freq)
 			  step_freq(_step_freq),
 			  ball(this->world, 0, 0, ball_size::MED_BALL) {
 	
-	YAML::Node config = YAML::LoadFile(".config.yml");	
+	YAML::Node config = YAML::LoadFile(".rl_config.yml");	
 
 	this->length = config["camp_length"].as<float>();
 	this->height = config["camp_height"].as<float>();
